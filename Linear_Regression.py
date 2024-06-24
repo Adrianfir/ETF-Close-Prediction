55baac6d52e4e865020baa53cb2c3532e59973d9
Adrianfir <pfirouzmakan@gmail.com>
2024-06-24 18:47:36 -0400

    midifications and developments

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import util.util as util
 from sklearn.model_selectrion import train_test_split
-from sklearn.preprocessing import StandardSclaer
+from sklearn.preprocessing import PolynomialFeatures, StandardSclaer
+from sklearn.linear_model import LinearRegressopn, ElasticNet
+from sklearn.pipeline import Pipeline
 
 
 
@@ -20,6 +22,19 @@ print(df.is_null())
 print(df.head(2))
 print(df.describe().transpose())
 util.visualization(df)
+
+x, y = util.feat_label(df)
+x = PolynomialFeatures(x, degree=config["degree"])
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = config['test_size'], 
+	random_state = config['seed'])
+
+
+
+
+
+
+
+
 
 
 
